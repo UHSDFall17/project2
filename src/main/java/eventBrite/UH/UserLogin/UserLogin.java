a80a9920a640cba2b8862d67b6fae253a199f065
Dustmaner <erick.soy@hotmail.com>
2017-10-29 19:03:23 -0500

    .

--- src/main/java/eventBrite/UH/UserLogin/UserLogin.java
+++ src/main/java/eventBrite/UH/UserLogin/UserLogin.java
@@ -1,6 +1,13 @@
 package eventBrite.UH.UserLogin;
 
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.List;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class UserLogin {
 
@@ -13,17 +20,40 @@ public class UserLogin {
     public int Login()
     {
         String userName;
+        String passwordInput;
         String password;
         try {
-            System.out.println("Input your User Name");
+            System.out.println("Input your User Name"); //USER
             userName=sc.nextLine();
 
             /*CHECK IF USER EXISTS*/
+            Path userBase = Paths.get("User_db.json");
+            Charset charset = Charset.forName("ISO-8859-1");
+            try {
+                List<String> lines = Files.readAllLines(userBase, charset);
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+            }catch (IOException e) {
+                System.out.println(e);
+            }
 
-            System.out.println("Input your Password");
-            password=sc.nextLine();
+            /*When the string in front of "uName": == userName make the password = corresponding "pWord":*/
+
+            System.out.println("Input your Password"); //PASSWORD
+            passwordInput=sc.nextLine();
+
 
             /*CHECK IF USER AND PASSWORD MATCH*/
+            /*if (passwordInput.equals(password))
+            {
+                System.out.println("Thanks for Logging in");
+            }
+            else
+            {
+                System.out.println("User and Password do not match!");
+            }*/
+
 
 
 
