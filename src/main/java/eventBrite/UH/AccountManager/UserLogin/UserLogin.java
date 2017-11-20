0c320d2cfa365a685e9c1e0180940d231220b1c5
Dustmaner <erick.soy@hotmail.com>
2017-11-19 20:45:40 -0600

    Fully functional User and Password input match.

--- src/main/java/eventBrite/UH/AccountManager/UserLogin/UserLogin.java
+++ src/main/java/eventBrite/UH/AccountManager/UserLogin/UserLogin.java
@@ -22,6 +22,12 @@ public class UserLogin {
         String userName;
         String passwordInput;
         String password;
+        int userFound = 0;
+        int passFound = 0;
+        int lineCount = 0;
+        int liner = 0;
+        int startIndex;
+        int endIndex;
         try {
             System.out.println("Input your User Name"); //USER
             userName=sc.nextLine();
@@ -32,7 +38,25 @@ public class UserLogin {
             try {
                 List<String> lines = Files.readAllLines(userBase, charset);
                 for (String line : lines) {
-                    System.out.println(line);
+                    //System.out.println(line);
+                    line = line.replace("{\"uName\":\"","");
+                    endIndex = line.indexOf("\"");
+                    line = line.substring(0,endIndex);
+                    if (userName.equals(line)){
+                        //System.out.println("Correct User: " + userName);
+                        userFound = 1;
+                        liner = lineCount;
+                    }
+                    /*else{
+                        System.out.println("Incorrect User: "+ line);
+                        System.exit(1);
+                    }*/
+                    lineCount += 1;
+                }
+                if (userFound == 0)
+                {
+                    System.out.println("User Not Found: "+ userName);
+                    System.exit(1);
                 }
             }catch (IOException e) {
                 System.out.println(e);
@@ -40,9 +64,37 @@ public class UserLogin {
 
             /*When the string in front of "uName": == userName make the password = corresponding "pWord":*/
 
+            lineCount = 0;
             System.out.println("Input your Password"); //PASSWORD
             passwordInput=sc.nextLine();
 
+
+            try {
+                List<String> lines = Files.readAllLines(userBase, charset);
+                for (String line : lines) {
+                    //System.out.println(line);
+                    startIndex = line.indexOf("pWord\":\"")+8;
+                    //System.out.println("line == " + line + "\nstartIndex == " + startIndex);
+                    endIndex = line.indexOf("\"}");
+                    line = line.substring(startIndex,endIndex);
+                    if (passwordInput.equals(line) && lineCount == liner){
+                        System.out.println("Login Successful");
+                        passFound = 1;
+                    }
+                    else{
+                        //System.out.println("Expected Pass: "+ line);
+                        //System.exit(1);
+                    }
+                    lineCount += 1;
+                }
+                if (passFound == 0)
+                {
+                    System.out.println("Password does not match: "+ passwordInput);
+                    System.exit(1);
+                }
+            }catch (IOException e) {
+                System.out.println(e);
+            }
 
             /*CHECK IF USER AND PASSWORD MATCH*/
             /*if (passwordInput.equals(password))
