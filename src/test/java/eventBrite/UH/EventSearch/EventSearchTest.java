29cef733f00c202155af72d8d389050f5e30a610
nourSmaoui <smaoui.nour@gmail.com>
2017-11-28 10:50:47 -0800

    Unit testing - edit search tests to handle database testing confitions

--- src/test/java/eventBrite/UH/EventSearch/EventSearchTest.java
+++ src/test/java/eventBrite/UH/EventSearch/EventSearchTest.java
@@ -1,5 +1,6 @@
 package eventBrite.UH.EventSearch;
 
+import eventBrite.UH.DatabaseManager.DatabaseHandler;
 import eventBrite.UH.EventManager.EventSearch;
 import eventBrite.UH.EventTools.EventInputScanner;
 import eventBrite.UH.EventTools.EventTypes;
@@ -26,110 +27,142 @@ public class EventSearchTest {
 
     @Test
     public void searchEventTestSearchOnly() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch1"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch1"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
     @Test
     public void searchEventTestSearchAgain() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch2"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch2"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
     @Test
     public void searchEventTestSearchByTitle() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch3"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch3"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
     @Test
     public void searchEventTestSearchByTitleNotFound() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch6"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch6"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
     @Test
     public void searchEventTestSearchByLocation() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch4"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch4"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET, ret);
+        assertEquals(EventTypes.Return.SUCCESS, ret);
     }
 
     @Test
     public void searchEventTestSearchByLocationNotFound() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch7"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch7"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET, ret);
+        assertEquals(EventTypes.Return.SUCCESS, ret);
     }
 
     @Test
     public void searchEventTestSearchByPriceRange() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch5"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch5"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
     @Test
     public void searchEventTestSearchByPriceRangeNotFound() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch8"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch8"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
     @Test
     public void searchEventTestExitSearch() throws FileNotFoundException {
-        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testEventSearch9"));
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch9"));
         EventInputScanner.setScanner(scanner);
 
         EventTypes.Return ret = EventTypes.Return.SUCCESS;
 
         ret = es.searchEvent();
 
-        assertEquals(EventTypes.Return.RESET,ret);
+        assertEquals(EventTypes.Return.SUCCESS,ret);
+    }
+
+    @Test
+    public void searchEventEmptyNumber() throws FileNotFoundException {
+        DatabaseHandler.setDB("project2");
+        DatabaseHandler.setUser("root");
+        Scanner scanner = new Scanner(new FileInputStream("./src/test/testInputs/testsEventSearch/testEventSearch10"));
+        EventInputScanner.setScanner(scanner);
+
+        EventTypes.Return ret = EventTypes.Return.SUCCESS;
+
+        ret = es.searchEvent();
+
+        assertEquals(EventTypes.Return.SUCCESS,ret);
     }
 
 
